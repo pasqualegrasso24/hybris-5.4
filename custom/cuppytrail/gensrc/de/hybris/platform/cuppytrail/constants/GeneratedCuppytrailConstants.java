4e41c72b2cea7c575f30692f85509bcc05eded1c
Pasquale Grasso <grassopasquale24@libero.it>
2017-04-27 20:59:29 +0200

    Trail New_Data_Model Integration in hMC

--- custom/cuppytrail/gensrc/de/hybris/platform/cuppytrail/constants/GeneratedCuppytrailConstants.java
+++ custom/cuppytrail/gensrc/de/hybris/platform/cuppytrail/constants/GeneratedCuppytrailConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 27-apr-2017 0.15.14                         ---
+ * --- Generated at 27-apr-2017 12.38.00                        ---
  * ----------------------------------------------------------------
  *  
  * [y] hybris Platform
@@ -25,6 +25,37 @@ package de.hybris.platform.cuppytrail.constants;
 public class GeneratedCuppytrailConstants
 {
 	public static final String EXTENSIONNAME = "cuppytrail";
+	public static class TC
+	{
+		public static final String STADIUM = "Stadium".intern();
+		public static final String STADIUMACCESS = "StadiumAccess".intern();
+		public static final String STADIUMTYPE = "StadiumType".intern();
+	}
+	public static class Attributes
+	{
+		public static class Match
+		{
+			public static final String STADIUM = "stadium".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class StadiumAccess
+		{
+			public static final String ROAD = "road".intern();
+			public static final String RAIL = "rail".intern();
+			public static final String PLANE = "plane".intern();
+		}
+		public static class StadiumType
+		{
+			public static final String OPENAIR = "openair".intern();
+			public static final String ENCLOSED = "enclosed".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String STADIUMMATCHRELATION = "StadiumMatchRelation".intern();
+	}
 	
 	protected GeneratedCuppytrailConstants()
 	{
